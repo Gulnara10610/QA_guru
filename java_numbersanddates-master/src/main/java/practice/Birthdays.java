cf8d8de07024c0c49f1aeb3d99263c9db07d11fe
79818 <gulnara0706@yandex.ru>
2022-08-29 18:50:48 +0300

    Test HomeWork #2

--- java_numbersanddates-master/src/main/java/practice/Birthdays.java
+++ java_numbersanddates-master/src/main/java/practice/Birthdays.java
@@ -1,5 +1,12 @@
 package practice;
 
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
+import java.util.Objects;
+
 public class Birthdays {
 
     public static void main(String[] args) {
@@ -8,16 +15,32 @@ public class Birthdays {
         int month = 12;
         int year = 1990;
 
-        System.out.println(collectBirthdays(year, month, day));
+        String text = "";
+        LocalDate date;
+
+
+
 
     }
 
     public static String collectBirthdays(int year, int month, int day) {
+        int i= 0;
+        LocalDate birthday = LocalDate.of(1992, 07,22);
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - EE", Locale.ENGLISH);
+        System.out.println(birthday.until(today, ChronoUnit.YEARS));
 
-        //TODO реализуйте метод для построения строки в следующем виде
-        //0 - 31.12.1990 - Mon
-        //1 - 31.12.1991 - Tue
-        
-        return "";
+        String text = "";
+        while (birthday.isBefore(today)){
+            birthday = birthday.plusYears(1);
+            System.out.println(formatter.format(birthday));
+        }
+        return text + birthday;
+
+
     }
 }
+
+
+
+
