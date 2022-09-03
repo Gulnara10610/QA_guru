7999a4ae3f1fe911c4334fa0359da54f08df5253
79818 <gulnara0706@yandex.ru>
2022-09-03 17:37:21 +0300

    Test HomeWork #3.4

--- Mark/src/test/java/Homework.java
+++ Mark/src/test/java/Homework.java
@@ -96,6 +96,98 @@ public class Homework {
         var radioButton =  driver.findElements(By.className("fake-radiobutton"));
         Assert.assertEquals("Неправильный текст", 5, radioButton.size());
     }
+    private By emailLocator = By.className("email");
+    private By writeToMeLocator = By.id("write-to-me");
+    private By resultEmailLocator = By.className("notification-email");
+    @Test
+    public void testKitten(){
+        driver.navigate().to("http://qajava.skillbox.ru/module04/lesson2/changed.html");
+        var email = "test@test.ru";
+        driver.findElement(emailLocator).sendKeys(email);
+        driver.findElement(writeToMeLocator).click();
+        var resultEmailElement = driver.findElement(By.className("notification-email"));
+        Assert.assertTrue("Не отобразился текст ", driver.findElement(resultEmailLocator).isDisplayed());
+        Assert.assertEquals("Не отобразился текст ", email, resultEmailElement.getText());
+
+    }
+
+    @Test
+    public void testKittensEmptyEmail(){
+        driver.navigate().to("http://qajava.skillbox.ru/module04/lesson2/changed.html");
+        driver.findElement(writeToMeLocator).click();
+
+        Assert.assertEquals("Не отобразился текст ", "", driver.findElement(resultEmailLocator).getText());
+    }
+
+    private By radioButtonBoy = By.id("boy");
+    private By radioButtonGirl = By.id("girl");
+    private By inputEmail = By.name("email");
+    private By button = By.id("sendMe");
+    private By resultText = By.className("your-email");
+    private By anotherEmail = By.id("anotherEmail");
+
+    @Test
+    public void testDogName() {
+        driver.navigate().to("http://qajava.skillbox.ru/module04/lesson2/practice/");
+        driver.findElement(radioButtonBoy).click();
+        var email = "testing@test.ru";
+        driver.findElement(inputEmail).sendKeys(email);
+        driver.findElement(button).click();
+        Assert.assertTrue("не отображается текст", driver.findElement(resultText).isDisplayed());
+        Assert.assertEquals("неправильный текст", email, driver.findElement(resultText).getText());
+    }
+    @Test
+    public void testDogAnotherEmail() {
+        driver.navigate().to("http://qajava.skillbox.ru/module04/lesson2/practice/");
+        driver.findElement(radioButtonGirl).click();
+        var email2 ="Admin";
+        driver.findElement(inputEmail).sendKeys(email2);
+        driver.findElement(button).click();
+        Assert.assertEquals("не отображается емаил", email2, driver.findElement(resultText).getText());
+
+
+    }
+
+
+    @Test
+    public void testFeedBack(){
+        driver.navigate().to("http://qajava.skillbox.ru/module04/lesson3/os.html");
+        var nameLocator = By.cssSelector(".data.text");
+        var buttonLocator = By.id("comment");
+        var messageLocator = By.className("message-header");
+        driver.findElement(nameLocator).sendKeys("Ivan");
+        driver.findElement(buttonLocator).click();
+        Assert.assertTrue("не отобразился текст", driver.findElement(messageLocator).isDisplayed());
+    }
+    @Test
+    public void testFeedBackEmail(){
+        driver.navigate().to("http://qajava.skillbox.ru/module04/lesson3/os.html");
+        var inputEmailLocator = By.cssSelector(".data.field");
+        var buttonLocator = By.id("comment");
+        var messageLocator = By.className("message-header");
+        driver.findElement(inputEmailLocator).sendKeys("qwert@test.ru");
+        var textFieldLocator = By.cssSelector(".field.text");
+        driver.findElement(textFieldLocator).sendKeys("something");
+        driver.findElement(buttonLocator).click();
+        Assert.assertTrue("не указана почта", driver.findElement(messageLocator).isDisplayed());
+
+    }
+
+
+    @Test
+    public void testHomeWork(){
+        driver.navigate().to("http://qajava.skillbox.ru/module04/lesson3/os.html");
+        var inputName = By.cssSelector(".form-input.fio");
+        var inputStreet = By.cssSelector(".data.form-input.street");
+        var inputHouse = By.cssSelector(".form-input.house");
+        var inputFlat = By.cssSelector(".form-input.flat");
+        var inputDate = By.cssSelector(".form-input.date");
+        var buttonCallMaster = By.cssSelector(".form-submit");
+
+        }
+    }
+
+
 
 
 
