5faa975d1963730bd8f684e18ec6058c50d1b4ae
79818 <gulnara0706@yandex.ru>
2022-09-10 10:41:03 +0300

    6.5

--- Mark/src/test/java/FunnySocks.java
+++ Mark/src/test/java/FunnySocks.java
@@ -5,14 +5,20 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class FunnySocks {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
         driver =  new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
     }
 
@@ -130,6 +136,77 @@ public class FunnySocks {
         driver.findElement(menuList);
         driver.findElement(fiveTask);
     }
+
+
+
+
+    @Test
+    public void dailyTest(){
+        driver.navigate().to("http://qa.skillbox.ru/module15/bignotes/#/");
+        var buttonPlus = By.cssSelector("button.pageCreate__sidebarHeaderAdd > svg:nth-of-type(1)");
+        driver.findElement(buttonPlus).click();
+        var lightBox = By.cssSelector("div.popup");
+        wait.until(ExpectedConditions.presenceOfElementLocated(lightBox));
+        driver.findElement(lightBox);
+        var baseInput = By.cssSelector("div.baseInput.popup__baseInput > input:first-child");
+        driver.findElement(baseInput).sendKeys("заголовок новой записи");
+        var textarea = By.cssSelector("div.baseTextarea.popup__textarea textarea.baseTextarea__text");
+        driver.findElement(textarea).sendKeys("текст новой записи");
+        var addButton = By.cssSelector("div.popup__content button.baseButton.popup__baseButton");
+        driver.findElement(addButton).click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(lightBox));
+        var text1 = By.cssSelector("div.articlePreview.pageCreate__articlePreview:nth-child(1) > div.articlePreview__content > *:nth-child(2)");
+
+        Assert.assertEquals("заголовок новой записи",driver.findElement(text1).getText());
+        var textText = By.cssSelector("div.articlePreview.pageCreate__articlePreview:nth-child(1) > div.articlePreview__content > *:nth-child(3)");
+        Assert.assertEquals("текст новой записи", driver.findElement(textText).getText());
+
+
+    }
+    @Test
+    public void homeWorkLocator() {
+        var locatorHomeWork = By.cssSelector("section.header p");
+        var locatorHomeWorkP = By.cssSelector("div.mainText.text a");
+        var locatorA = By.cssSelector("div.text a");
+        var testLocatorHref = By.cssSelector("div.text a");
+
+
+    }
+
+    @Test
+    public void testSecond(){
+        driver.navigate().to("https://lm.skillbox.cc/qa_tester/module04/practice1/");
+        var secondTask = By.cssSelector("input[type='text']:not([id])");
+        driver.findElement(secondTask);
+        var errorLocator = By.cssSelector("p[class^='form']:not([class$='error'])");
+        driver.findElement(errorLocator);
+        var locatorForm = By.cssSelector("div.form-inner > form.form > p:nth-of-type(1)");
+        driver.findElement(locatorForm);
+    }
+
+    @Test
+    public void tasskFird(){
+        driver.navigate().to("http://qa.skillbox.ru/module15/bignotes/#/statistic");
+        var buttonLink = By.cssSelector("div.articlePreview.pageStatistic__article:nth-of-type(1) > button.articlePreview__link");
+        driver.findElement(buttonLink).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        var allTexts = By.cssSelector("div.pageArticle__sidebarHeader.pageArticle__sidebarWrapper");
+
+        Assert.assertEquals("Неправильный текст","Все записи",driver.findElement(allTexts).getText());
+        var basketClick = By.cssSelector("button.pageArticle__button:nth-of-type(1) > svg.pageArticle__icon > use");
+        driver.findElement(basketClick).click();
+        var firstText = By.cssSelector("div.articlePreview.pageArticle__articlePreview:nth-of-type(1) button.articlePreview__link");
+        driver.findElement(firstText).click();
+        driver.findElement(basketClick).click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(firstText));
+        var invisibleElement = By.cssSelector("(div.vb-content");
+        Assert.assertTrue("Неправильно указан текст", driver.findElement(invisibleElement).isEnabled());
+
+    }
+
+
+
+
 
 
 
