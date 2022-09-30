b74de7f23c71be63b99dca7c2511bc20d71eeba4
Gulnara <gulnara0706@yandex.ru>
2022-09-30 12:39:51 +0300

    ready second part

--- java_numbersanddates-master/src/main/java/practice/Birthdays.java
+++ java_numbersanddates-master/src/main/java/practice/Birthdays.java
@@ -18,6 +18,8 @@ public class Birthdays {
         String text = "";
         LocalDate date;
 
+        System.out.println(collectBirthdays(year,month,day));
+
 
 
 
@@ -25,20 +27,26 @@ public class Birthdays {
 
     public static String collectBirthdays(int year, int month, int day) {
         int i= 0;
-        LocalDate birthday = LocalDate.of(1992, 07,22);
+        LocalDate birthday = LocalDate.of(year,month,day);
         LocalDate today = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - EE", Locale.ENGLISH);
         System.out.println(birthday.until(today, ChronoUnit.YEARS));
 
-        String text = "";
+        String text = System.lineSeparator();
         while (birthday.isBefore(today)){
+
+            i = i +1;
+            text = text + i + "-" + formatter.format(birthday) + System.lineSeparator();
             birthday = birthday.plusYears(1);
-            System.out.println(formatter.format(birthday));
         }
-        return text + birthday;
+        if(birthday.isEqual(today)){
+            text = i + "-" + formatter.format(today);
+        }
+        return  text ;
 
 
     }
+
 }
 
 
